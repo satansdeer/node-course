aa3fda8adc7bc82e7f72ce4b4e067afed5aaaf93
Maksim Ivanov <satansdeer@gmail.com>
2020-01-02 19:27:53 +0100

    Change launch port

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-const app = require('./app')
+const app = require("./app");
 
-app.listen(3000, () => {
-    console.log('Example app listening on port 3000!');
-})
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
